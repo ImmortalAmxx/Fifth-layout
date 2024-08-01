a4582cc83b5fb069e88be65b29d368c06862a6d1
ImmortalAmxx <bankaitensazangetsu1337@gmail.com>
2024-08-01 17:55:28 +0300

    Added styles, added bootstrap v4, created the first version of the header.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
+// Завдання для компіляції SCSS в CSS
 gulp.task('styles', function() {
-  return gulp.src('scss/**/*.scss')
+  return gulp.src('scss/**/*.scss') // Вказати правильний шлях до SCSS файлів
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('css')); // Вказати правильний шлях до вихідної папки
 });
 
+// Завдання для відслідковування змін
 gulp.task('watch', function() {
-  gulp.watch('src/scss/**/*.scss', gulp.series('styles'));
+  gulp.watch('scss/**/*.scss', gulp.series('styles'));
 });
 
+// Завдання за замовчуванням, яке запускає компіляцію та відслідковування
 gulp.task('default', gulp.series('styles', 'watch'));
