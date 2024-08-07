c9903a5312d294212a11b8b49af7009473306e65
ImmortalAmxx <bankaitensazangetsu1337@gmail.com>
2024-08-07 17:19:23 +0300

    Added hover effect for product block (blue lines).

--- js/app.js
+++ js/app.js
@@ -1,20 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const burgerMenu = document.querySelector('.burger-menu');
-  const burgerIcon = document.querySelector('.burger-icon');
-  const navItem = document.querySelector('.nav-item');
-
-  burgerMenu.addEventListener('click', function() {
-    navItem.classList.toggle('active');
-    burgerIcon.classList.toggle('active');
+$(document).ready(function() {
+  $('.burger-menu').on('click', function() {
+    $('.nav-item').toggleClass('active');
+    $('.burger-icon').toggleClass('active');
   });
 
-  var header = document.getElementById('header');
-
-  window.addEventListener('scroll', function () {
-    if (window.scrollY > 50) {
-      header.classList.add('scrolled');
+  $(window).on('scroll', function() {
+    if ($(window).scrollTop() > 50) {
+      $('#header').addClass('scrolled');
     } else {
-      header.classList.remove('scrolled');
+      $('#header').removeClass('scrolled');
     }
   });
 });
