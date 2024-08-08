4ae3f0a86c41d795681732389c9a6a4294ae77f2
ImmortalAmxx <bankaitensazangetsu1337@gmail.com>
2024-08-08 10:22:44 +0300

    Changed the navigation menu (changed the background color, added animation after opening the menu, changed the position of the buttons in the menu).

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
 $(document).ready(function() {
   $('.burger-menu').on('click', function() {
-    $('.nav-item').toggleClass('active');
-    $('.burger-icon').toggleClass('active');
+    var $navItem = $('.nav-item');
+    var $burgerIcon = $('.burger-icon');
+
+    if ($navItem.hasClass('active')) {
+      $navItem.removeClass('active');
+      setTimeout(function() {
+        $navItem.css('display', 'none');
+      }, 300);
+    } else {
+      $navItem.css('display', 'flex');
+      setTimeout(function() {
+        $navItem.addClass('active');
+      }, 10);
+    }
+
+    $burgerIcon.toggleClass('active');
   });
 
   $(window).on('scroll', function() {
